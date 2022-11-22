f38e3e2657c743289be56e941ebab3aa3cc523df
韩丽华 <2672889048@qq.com>
2022-11-22 15:08:27 +0800

    完成了登錄功能

---
+++ src/router.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '@/components/Login'
+import Home from '@/components/Home' 
+
+Vue.use(VueRouter)
+
+const routes = [
+  
+]
+
+const router = new VueRouter({
+  routes :[
+    {path:'/',redirect:'/login'},
+    {path:'/login',component:Login},
+    {path:'/home',component:Home }
+  ]
+})
+
+// 挂载路由道行守卫
+router.beforeEach((to,from,next)=>{
+  // to 将要访问的路径
+  // from 代表从哪个路径跳转而来
+  // next 是一个函数，表示放行
+  // next() 直接放行  next('/login') 强制跳转
+  if(to.path === '/login') return next()
+  const tokenStr = window.sessionStorage.getItem('token')
+  if(!tokenStr) return next('/login')
+  next()
+})
+
+export default router
