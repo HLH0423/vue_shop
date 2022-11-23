c1cdd6d6e5fa2ea22e7c3ee451f890ab75f19c15
韩丽华 <2672889048@qq.com>
2022-11-23 16:44:13 +0800

    完成用户列表功能的开发

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,142 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+  <!-- 头部区域 -->
+  <el-header>
+      <div>
+          <img src="../assets/heima.png" alt="">
+          <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+  </el-header>
+  <!-- 页面主体区域 -->
+  <el-container>
+      <!-- 侧边栏 -->
+    <el-aside :width="isCollapse ? '64px' : '200px' ">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <!-- 侧边栏菜单区域 -->
+         <el-menu background-color="#333744" text-color="#fff" 
+         active-text-color="#409EFF" unique-opened :collapse="isCollapse"
+         :collapse-transition="false" :router="true" :default-active="activePath">
+             <!-- 一级菜单 -->
+            <el-submenu :index="item.id+''" v-for="item in menulist" :key="item.id">
+                <!-- 一级菜单的模板区域 -->
+                <template slot="title">
+                <!-- 图标 -->
+                <i :class="iconObj[item.id]"></i>
+                <!-- 文本 -->
+                <span>{{item.authName}}</span>
+                </template>
+                <!-- 二级菜单 -->
+                <el-menu-item :index="'/'+subItem.path" v-for="subItem in item.children" 
+                :key="subItem.id" @click="saveNavState('/'+subItem.path)">
+                    <!-- 二级菜单的模板区域 -->
+                    <template slot="title">
+                    <!-- 图标 -->
+                    <i class="el-icon-menu"></i>
+                    <!-- 文本 -->
+                    <span>{{subItem.authName}}</span>
+                    </template>
+                </el-menu-item>
+            </el-submenu>
+        </el-menu>
+    </el-aside>
+    <!-- 右侧内容主体 -->
+    <el-main>
+       <router-view></router-view>
+    </el-main>
+  </el-container>
+</el-container>
 </template>
 
 <script>
 export default {
+  data() {
+      return {
+          menulist:[],
+          iconObj:{
+              '125':'iconfont icon-users',
+              '103':'iconfont icon-tijikongjian',
+              '101':'iconfont icon-shangpin',
+              '102':'iconfont icon-danju',
+              '145':'iconfont icon-baobiao',
+          },
+          isCollapse:false,
+          activePath:''
+      }
+  },
+
+  created() {
+      this.getMenuList()
+      this.activePath = window.sessionStorage.getItem('activePath')
+  },
+  
   methods: {
     logout() {
       window.sessionStorage.clear()
       this.$router.push('/login')
     },
+
+    async getMenuList() {
+        const {data:res} = await this.$http.get('menus')
+        if(res.meta.status !== 200) return this.$message.error(res.meta.msg)
+        this.menulist = res.data
+        console.log(res);
+    },
+
+    // 点击按钮，切换菜单的折叠
+    toggleCollapse() {
+        this.isCollapse = !this.isCollapse
+    },
+
+    saveNavState(activePath) {
+        window.sessionStorage.setItem('activePath',activePath)
+        this.activePath = activePath
+    }
   },
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+    .home-container {
+        height: 100%;
+    }
+    .el-header {
+        background-color: #373d41;
+        display: flex;
+        justify-content: space-between;
+        padding-left: 0;
+        align-items: center;
+        color: #fff;
+        font-size: 20px;
+        > div {
+            display: flex;
+            align-items: center;
+            span {
+                margin-left: 15px;
+            }
+        }
+    }
+    .el-aside {
+        background-color: #333744;
+        .el-menu {
+            border-right: none;
+        }
+    }
+    .el-main {
+        background-color: #eaedf1;
+    }
+
+    .iconfont {
+        margin-right: 10px;
+    }
+
+    .toggle-button {
+        background-color: #4A5064;
+        font-size: 10px;
+        line-height: 24px;
+        color: #fff;
+        text-align: center;
+        letter-spacing: 0.2em;
+        cursor: pointer;
+    }
+</style>
--- src/router.js
+++ src/router.js
@@ -1,31 +1,39 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '@/components/Login'
-import Home from '@/components/Home' 
+import Home from '@/components/Home'
+import Welcome from '@/components/Welcome'
+import Users from '@/components/user/Users'
 
 Vue.use(VueRouter)
 
-const routes = [
-  
-]
+const routes = []
 
 const router = new VueRouter({
-  routes :[
-    {path:'/',redirect:'/login'},
-    {path:'/login',component:Login},
-    {path:'/home',component:Home }
-  ]
+  routes: [
+    { path: '/', redirect: '/login' },
+    { path: '/login', component: Login },
+    {
+      path: '/home',
+      component: Home,
+      redirect: '/welcome',
+      children: [
+        { path: '/welcome', component: Welcome },
+        { path: '/users', component: Users },
+      ],
+    },
+  ],
 })
 
 // 挂载路由道行守卫
-router.beforeEach((to,from,next)=>{
+router.beforeEach((to, from, next) => {
   // to 将要访问的路径
   // from 代表从哪个路径跳转而来
   // next 是一个函数，表示放行
   // next() 直接放行  next('/login') 强制跳转
-  if(to.path === '/login') return next()
+  if (to.path === '/login') return next()
   const tokenStr = window.sessionStorage.getItem('token')
-  if(!tokenStr) return next('/login')
+  if (!tokenStr) return next('/login')
   next()
 })
 
