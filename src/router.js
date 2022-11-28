21eaf5863e5a9788f6682dbcdfdc412b41c1e352
韩丽华 <2672889048@qq.com>
2022-11-28 18:14:00 +0800

    完成了权限功能的开发

--- src/router.js
+++ src/router.js
@@ -4,6 +4,8 @@ import Login from '@/components/Login'
 import Home from '@/components/Home'
 import Welcome from '@/components/Welcome'
 import Users from '@/components/user/Users'
+import Rights from '@/components/power/Rights'
+import Roles from '@/components/power/Roles'
 
 Vue.use(VueRouter)
 
@@ -20,6 +22,8 @@ const router = new VueRouter({
       children: [
         { path: '/welcome', component: Welcome },
         { path: '/users', component: Users },
+        { path: '/rights', component: Rights },
+        { path: '/roles', component: Roles },
       ],
     },
   ],
