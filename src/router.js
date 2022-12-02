ed9e2f5d4ae285091f811dfc8ef05de4a3c29833
韩丽华 <2672889048@qq.com>
2022-12-02 09:08:48 +0800

    完成商品功能开发

--- src/router.js
+++ src/router.js
@@ -8,6 +8,8 @@ import Rights from '@/components/power/Rights'
 import Roles from '@/components/power/Roles'
 import Cate from '@/components/goods/Cate'
 import Params from '@/components/goods/Params'
+import GoodsList from '@/components/goods/List'
+import Add from '@/components/goods/Add'
 
 Vue.use(VueRouter)
 
@@ -28,6 +30,8 @@ const router = new VueRouter({
         { path: '/roles', component: Roles },
         { path: '/categories', component: Cate },
         { path: '/params', component: Params },
+        { path: '/goods', component: GoodsList },
+        { path: '/goods/add', component: Add },
       ],
     },
   ],
