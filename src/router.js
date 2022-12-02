426b20c26a0a2dd795a0af2a0697cebf5ecc7acb
韩丽华 <2672889048@qq.com>
2022-12-02 20:58:50 +0800

    完成订单功能开发

--- src/router.js
+++ src/router.js
@@ -10,6 +10,7 @@ import Cate from '@/components/goods/Cate'
 import Params from '@/components/goods/Params'
 import GoodsList from '@/components/goods/List'
 import Add from '@/components/goods/Add'
+import Order from '@/components/order/Order'
 
 Vue.use(VueRouter)
 
@@ -32,6 +33,7 @@ const router = new VueRouter({
         { path: '/params', component: Params },
         { path: '/goods', component: GoodsList },
         { path: '/goods/add', component: Add },
+        { path: '/orders', component: Order },
       ],
     },
   ],
