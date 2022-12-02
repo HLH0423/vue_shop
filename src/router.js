759ef0331eb8438909d7c0ad6a59f24b6aa882cb
韩丽华 <2672889048@qq.com>
2022-12-02 21:25:14 +0800

    完成了报表功能

--- src/router.js
+++ src/router.js
@@ -11,6 +11,7 @@ import Params from '@/components/goods/Params'
 import GoodsList from '@/components/goods/List'
 import Add from '@/components/goods/Add'
 import Order from '@/components/order/Order'
+import Report from '@/components/report/Report'
 
 Vue.use(VueRouter)
 
@@ -34,6 +35,7 @@ const router = new VueRouter({
         { path: '/goods', component: GoodsList },
         { path: '/goods/add', component: Add },
         { path: '/orders', component: Order },
+        { path: '/reports', component: Report },
       ],
     },
   ],
